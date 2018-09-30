2432be4784fb1f755eb781730dfb34fe0fb77a8b
YHYeoooooooong <denebb97@gmail.com>
2018-09-30 23:32:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/example2_3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.example2_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1;
+
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",Toast.LENGTH_SHORT).show();}
+        });
+    }
+}
